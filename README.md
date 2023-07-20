9fead3f181f23608ecd94ca81272332842335a1e
webken00 <allenchinasa1@gmail.com>
2023-07-20 10:16:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# myTodoApp
+# myTodoApp
---
+++ asset/Js/script.js
@@ -0,0 +1,14 @@
+const inputText = document.getElementById("input-text");
+const listContainer = document.getElementById("list-container");
+
+function addNewTask(){
+    if(inputText.value === ''){
+        alert("Type New To do List!")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputText.value;
+        list-container.appendChild(li);
+    }
+
+}
